3bb27bb97917bb9085cf62254ad77c6066a89a75
vapatty6848 <vapatty@gmail.com>
2021-02-23 14:52:20 -0300

    iniciando projeto  store manager

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
+const express = require('express');
+const bodyParsser = require('body-parser');
+const { managerController } = require('./controller');
+
+const app = express();
+const PORT = 3000;
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use(bodyParsser.json());
+app.use('/manager', managerController);
+
+app.listen(PORT, () => console.log('App listening on PORT %s', PORT));
