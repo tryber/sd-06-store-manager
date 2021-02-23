bcdf454cb8ebf09204514cd3ce642bb48067cc49
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-02-23 14:52:09 -0300

    Starting project and adding connection

---
+++ src/models/connection.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGODB_URL = 'mongodb://127.0.0.1:27017';
+const DB_NAME = 'StoreManager';
+
+const connection = () => { 
+  return MongoClient.connect(MONGODB_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => { 
+      onsole.error(err);
+      process.exit();
+    });
+};
+
+module.exports = connection;
