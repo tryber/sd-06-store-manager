f651e22686bd8e2386046d7fd29f243d309ac70f
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-02-23 14:48:25 -0300

    first commit

---
+++ models/connection.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require('mongodb');
+
+//const MONGODB_URL = 'mongodb://127.0.0.1:27017';
+const MONGODB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DATABASE = 'StoreManager';
+
+const connection = () => MongoClient.connect(MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((conn) => conn.db(DATABASE))
+  .catch((_err) => {
+    process.exit();
+  });
+
+module.exports = connection;
