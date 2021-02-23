d7ab73f089504bed3fc5b852143e5152d9fdb2d2
ilanribeiro <ilanribeiro@gmail.com>
2021-02-23 16:35:01 -0300

    [ Feature ] Criei a conexão para subir o servidor, no arquivo index.js. Mantive o middleware existente, sem modificações.

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/hello', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`MyStoreApp active and listening on port ${port}!`));
