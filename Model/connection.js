0158a9d9b0385b81803787f3bebcb38cf272010b
labelle <beelsdelonge@gmail.com>
2021-02-23 18:52:34 -0300

    Solucionando erro no MONGODB_URL

--- Model/connection.js
+++ Model/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
 /* const MONGODB_URL = 'mongodb://localhost:27017/StoreManager'; */
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const MONGODB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 const DB_NAME = 'StoreManager';
 
