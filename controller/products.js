42bb889dcb07454f77b423a2b847cbc6175d75f2
Isa Koga <isadorakoga710@gmail.com>
2021-02-23 19:53:50 -0300

    :rotating_light: Add error req 2

--- controller/products.js
+++ controller/products.js
@@ -1,26 +1,41 @@
 const { Router } = require('express');
 
 const ProductsRouter = new Router();
-const { getAllProducts, createProduct, getProductById } = require('../modules/productModules');
+const {
+  getAllProducts,
+  createProduct,
+  getProductById
+} = require('../modules/productModules');
 const { validateProduct } = require('../services/prodServices');
 
 const twoHundred = 200;
 const twoHundredOne = 201;
+const fourHundredTwentyTwo = 422;
 
 ProductsRouter.post('/products', validateProduct, async (req, res) => {
   await createProduct(req.body);
-  res.status(twoHundredOne).json(req.body);
+  return res.status(twoHundredOne).json(req.body);
 });
 
 ProductsRouter.get('/products', async (req, res) => {
   const allProducts = await getAllProducts();
-  res.status(twoHundred).send(allProducts);
+  return res.status(twoHundred).send(allProducts);
 });
 
 ProductsRouter.get('/products/:id', async (req, res) => {
   const { id } = req.params;
+  const allProducts = await getAllProducts();
+  const existId = allProducts.find(product => product.id === id);
+  if (existId) {
+    return res.status(fourHundredTwentyTwo).json({
+      err: {
+        code: 'invalid_data',
+        message: 'Wrong id format',
+      },
+    });
+  }
   const productById = await getProductById(id);
-  res.status(twoHundred).send(productById);
+  return res.status(twoHundred).send(productById);
 });
 
 module.exports = { ProductsRouter };
