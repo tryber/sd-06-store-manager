7f604ec69b99bae5328076ec6d6fb3293f5bfd1d
Tiago Berwanger <berwangertiago@gmail.com>
2021-02-23 19:17:04 -0300

    Requisito 1

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ProductsController = require('./src/controllers/ProductsController');
+// const salesController = require('./src/controllers/SalesController');
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+// ________________________________________________________
+
+app.use(bodyParser.json());
+
+app.use('/products', ProductsController);
+
+// app.use('/sales', salesController);
+
+// ________________________________________________________
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log('running!'));
