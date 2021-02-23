8752b16c048c47144e30bfcff7c8e62fc485f4a8
Isa Koga <isadorakoga710@gmail.com>
2021-02-23 19:35:20 -0300

    :rotating_light: fix stats

--- controller/products.js
+++ controller/products.js
@@ -4,7 +4,7 @@ const ProductsRouter = new Router();
 const { getAllProducts, createProduct } = require('../modules/productModules');
 const { validateProduct } = require('../services/prodServices');
 
-const twoHundred = 200;
+const twoHundredOne = 201;
 
 ProductsRouter.get('/products', async (req, res) => {
   const allProducts = await getAllProducts();
@@ -13,7 +13,7 @@ ProductsRouter.get('/products', async (req, res) => {
 
 ProductsRouter.post('/products', validateProduct, async (req, res) => {
   await createProduct(req.body);
-  res.status(twoHundred).json(req.body);
+  res.status(twoHundredOne).json(req.body);
 });
 
 module.exports = { ProductsRouter };
