17995bf54eeb2290fcb64cd9da6049c3566515d9
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-23 14:28:05 -0300

    Iniciando o projeto

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on PORT ${PORT}!`));
