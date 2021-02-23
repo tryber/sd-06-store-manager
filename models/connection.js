3b571d0dcf90fefd8adcaf3c6f53a60722fb4fea
ilanribeiro <ilanribeiro@gmail.com>
2021-02-23 16:30:15 -0300

    [Feature] Criei a pasta models. Dentro dela, criei o arquivo connection.js para a conexão ao mongodb. também criei o arquivo .env para possibilitar a automatização da URL do banco na busca do avaliador

---
+++ models/connection.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+
+// Procedimento ensinado pelo Nato em uma thread de dúvida;
+const MONGODB_URL = process.env.IS_LOCAL
+  ? 'mongodb://localhost:27017/StoreManager'
+  : 'mongodb://127.0.0.1:27017';
+const DB_NAME = 'StoreManager';
+
+const connection = () => {
+  return MongoClient.connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
+};
+
+module.exports = connection;
