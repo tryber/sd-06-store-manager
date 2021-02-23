8646a17318e6c1fad286b157ba7492a54ca1cad1
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-02-23 18:46:22 -0300

    add express-rescue

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rescue = require('express-rescue');
 const app = express();
 const ProductsRouter = require('./controllers/ProductsController');
 const SalesRouter = require('./controllers/SalesController');
 
 app.use(bodyParser.json());
-const port = 3000;
+const port = 3000, NOT_FOUND = 404;
 
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.use('/products', ProductsRouter);
-app.use('/sales', SalesRouter);
+app.use('/products', rescue(ProductsRouter));
+app.use('/sales', rescue(SalesRouter));
+
+app.use('*', (req, res) => res.status(NOT_FOUND).json({ message: 'Page not found' }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
