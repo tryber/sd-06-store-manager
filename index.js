1cb1ffd9f18cf18471c97d1c7bd8fa188f9224ae
alcantara2509 <alcantara2509@gmail.com>
2021-02-23 15:55:10 -0300

    alcantara2509

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (_request, response) => {
-  response.send();
-});
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (_request, response) => response.send());
+
+app.listen(port)
