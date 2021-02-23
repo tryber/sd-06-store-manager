388758ad2338ab259b314dbdd30d5c6b9975d0c3
deko possas <andrepossaster@gmail.com>
2021-02-23 14:40:31 -0300

    criei as estruturas basicas do node com o JS

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+const express = require('express');
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use('');
+
+app.listen(port, () => console.log(`Listening to port ${port}`));
