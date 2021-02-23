4e2b8d742080471115cb52727382e1c11b1f907b
Anderson R <misterregisslv@gmail.com>
2021-02-23 14:40:13 -0300

    commit inicial store manager Allez

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`listen at port:${PORT}`));
