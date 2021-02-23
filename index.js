a9d560e2f14aa906cbfddf41b70945fd5e61cbbd
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2021-02-23 17:01:29 -0300

    initial config app on track

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+const SUCCESS = 200;
+
+app.get('/', (_req, res) => {
+  res.status(SUCCESS).send('Hello node');
+});
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log('Tô na porta 3000'));
