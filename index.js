35102064305af3a5ff5b2a50d30dc8083b0d424e
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-02-23 14:51:49 -0300

    workplace setup

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+const express = require('express');
+const routes = require('./routes');
+
+const app = express();
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+//
+
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
+
