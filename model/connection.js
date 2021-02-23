0d299bc727640690944856d0a085bcdca9a11426
isabellavjs <isabellavjs@gmail.com>
2021-02-23 14:27:53 -0300

    first commit

---
+++ model/connection.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require('mongodb');
+
+// const MONGODB_URL = 'mongodb://127.0.0.1:27017';
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+const connection = () => {
+  return MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then((conn) => conn.db(DB_NAME))
+    .catch((err) => {
+      console.error(err);
+      process.exit();
+    });
+};
+
+module.exports = connection;
