fa396c742d8a24b22dfd049c1b6f0d9bfe0c50e3
DehSartoreto <fixsartoreto@gmail.com>
2021-02-24 12:13:55 -0300

    return wrong

--- controller/products.js
+++ controller/products.js
@@ -8,14 +8,21 @@ const statusSucess = 200;
 const createSucess = 201;
 
 productsRouter.post('/', validateProduct, async (req, res) => {
-  const { name, quantity } = req.body;
-  const createdProduct = await productsModules.createProduct({name, quantity});
-  return res.status(createSucess).json(createdProduct);
+  await productsModules.createProduct(req.body);
+  res.status(createSucess).json(req.body);
 });
 
 productsRouter.get('/', async (_req, res) => {
   const allProducts = await productsModules.getAllProducts();
   res.status(statusSucess).json(allProducts);
+});
+
+productsRouter.get('/:id', async (_req, res) => {
+  const id = parseInt(req.params, 10);
+  const allProducts = await productsModules.getAllProducts();
+  const productFound = allProducts.find((product) => product.id === id);
+  
+  res.status(statusSucess).json(productFound);
 });
 
 module.exports = productsRouter;
--- modules/productsModules.js
+++ modules/productsModules.js
@@ -3,8 +3,8 @@ const connection = require('./connection');
 const getAllProducts = () => 
   connection().then((db) => db.collection('products').find().toArray());
 
-const createProduct = (name, quantity) =>
-  connection().then((db) => db.collection('products').insertOne({ name, quantity }));
+const createProduct = (data) =>
+  connection().then((db) => db.collection('products').insertOne(data));
 
 module.exports = {
   getAllProducts,
