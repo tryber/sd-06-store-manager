447e86b50494c361e02473e02f64bfbe38409a56
Júlia Pôrto <juportob@gmail.com>
2021-02-24 20:00:06 -0300

    Requisito 1 - fix code

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -3,7 +3,7 @@ const { createProductService } = require('../service/ProductsService');
 const { validateName, validateProduct } = require('../middlewares/productsMid');
 
 const router = Router();
-const SUCESS = 200;
+const SUCESS = 201;
 
 router.post('/', validateProduct, validateName, async (req, res) => {
   const { name, quantity } = req.body;
