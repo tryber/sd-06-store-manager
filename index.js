2136be925b2b52047acb4682c96aa404463508b1
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-02-24 16:28:39 -0300

    cria o index.js básico para testar se há sucesso na implementação da aplicação

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+const express = require('express');
+const app = express();
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`working, but don't push it... on PORT ${PORT} `));
