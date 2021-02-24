076e33b006c1408a1cc0e9771467fd44f826417e
rafaelgelatti <rafael.gelatti@gmail.com>
2021-02-23 22:14:32 -0300

    switching to remote

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,12 @@
 const { MongoClient } = require('mongodb');
 
 // local
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
-const DB_NAME = 'StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+// const DB_NAME = 'StoreManager';
 
 // remoto
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
-// const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
 
 const connection = () =>
   MongoClient.connect(MONGO_DB_URL, {
