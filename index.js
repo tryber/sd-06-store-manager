e099078ea79593b4b086b89c5e599ff7c29e4501
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-02-24 14:18:28 -0300

    req 1 correction

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { ProductsController } = require('./controllers');
-const { SalesController } = require('./controllers');
+const ProductsController = require('./controllers');
+//const SalesController = require('./controllers');
 const app = express();
 const PORT = 3000;
 
