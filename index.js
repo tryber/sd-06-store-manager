6a3964bf83ba87b6f8f243b8abf96f492762cba6
RenatoCostaDev <renatotrybe@gmail.com>
2021-02-24 15:02:21 -0300

    RenatoCostaDev => iniciando o projeto x

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const bell = 3000;
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.listen(bell, () => console.log(`For whom the ${bell} tolls!`));
