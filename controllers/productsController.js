bae48b7305a13f85c9f5c2eee99a311e831d3a73
Mariano Ariel Diaz Elsztain <made@poli.ufrj.br>
2021-02-24 15:58:03 -0300

    No magic numbers

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,29 +2,35 @@ const products = require('../services/products');
 const routes = require('express').Router();
 const rescue = require('express-rescue');
 
+const OK = 200;
+const BAD_REQUEST = 400;
+const NOT_FOUND = 404;
+
+
 
 routes.get('/:id', rescue(async (req, res) => {
   const { id } = req.params;
   const product = await products.findById(id);
 
-  if (!product) return res.status(404).json({ message: 'Not found' });
+  if (!product) return res.status(NOT_FOUND).json({ message: 'Not found' });
 
-  res.status(200).json(product);
+  res.status(OK).json(product);
 }));
 
 routes.route('/')
   .get(rescue(async (req, res) => {
     const products = await products.getAll();
 
-    res.status(200).json(products);
+    res.status(OK).json(products);
   }))
   .post(rescue(async (req, res) => {
     const { name, quantity } = req.body;
     const createdProduct = await products.create(name, quantity);
     
-    if (!createdProduct) return res.status(400).json({ message: 'Unsubmittable data' });
+    if (!createdProduct) return res.status(BAD_REQUEST)
+      .json({ message: 'Unsubmittable data' });
 
-    res.status(200).json(createdProduct);
+    res.status(OK).json(createdProduct);
   }));
 
 module.exports = routes;
