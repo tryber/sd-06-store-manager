95dc865489019eba156b0821e0f40f0817498297
Luiz E F Simoes <luizefsimoes@gmail.com>
2021-02-23 22:22:51 -0300

    Testando requisito 1

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,29 +5,29 @@ const created = 201;
 const invalidParams = 422;
 const zero = 0;
 
-const nameRefusedMsg = {
-  err: {
-    code: 'invalid_data',
-    message: '"name" length must be at least 5 characters long'
-  }
+const msgError = (string) => {
+  return {
+    err: {
+      code: 'invalid_data',
+      message: string,
+    }
+  };
 };
-const productExistingMsg = {
-  err: {
-    code: 'invalid_data',
-    message: 'Product already exists'
-  }
-};
+const nameRefusedMsg = (msgError('"name" length must be at least 5 characters long'));
+const productExistingMsg = (msgError('Product already exists'));
+const quantityGtZero = (msgError('"quantity" must be larger than or equal to 1'));
+const quantityNaN = (msgError('"quantity" must be a number'));
 
 const create = async (req, res) => {
   const { name, quantity } = req.body;
-  if (quantity <= zero) return res.status(invalidParams)
-    .json('"quantity" must be larger than or equal to 1');
-  if (!Number.isInteger(quantity)) return res.status(invalidParams)
-    .json('"quantity" must be a number');
+  if (quantity <= zero) return res.status(invalidParams).json(quantityGtZero);
+  // if (!Number.isInteger(quantity)) return res.status(invalidParams).json(quantityNaN);
+  if (typeof quantity !== 'number') return res.status(invalidParams).json(quantityNaN);
+  console.log(typeof quantity);
   const nome = service.nameValid(name, quantity);
   if(!nome) return res.status(invalidParams).json(nameRefusedMsg);
   const repeated = await service.productRepeat(name);
-  if(!repeated) return res.status(invalidParams).json(productExistingMsg);
+  if(!repeated) return res.status(invalidParams).send(productExistingMsg);
   const produto = await service.create(name, quantity);
   res.status(created).json(produto);
 };
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const unprocEntity =422;
-
 const controller = require('./controllers/controller');
 
 app.use(express.json());
