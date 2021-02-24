f9c7381bde2397c51a54a0bc2011c37097775ef1
rafMewtwo <ponciano.raf@gmail.com>
2021-02-24 16:54:57 -0300

    req1x10

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGODB_URL = 'mongodb://localhost:27017/StoreManager';
 // const MONGODB_URL = 'mongodb://127.0.0.1:27017';
 const DB_NAME = 'StoreManager';
 
