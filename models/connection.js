064778edf65838de200a312452f9bbe9f9e6ebe4
Júlia Pôrto <juportob@gmail.com>
2021-02-24 15:15:28 -0300

    Project Store Manager

---
+++ models/connection.js
@@ -0,0 +1,15 @@
+const { MongoClient } = require('mongodb');
+
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_NAME = 'StoreManager';
+
+const connection = async () => {
+  return await MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }).then((conn) => conn.db(DB_NAME)).catch((err) => {
+    process.exit();
+  });
+};
+
+module.exports = connection;
