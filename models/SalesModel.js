cc6951df60b9e929b7a6fe78b7df7e6bbf2c922b
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-02-24 19:22:12 -0300

    req 5

---
+++ models/SalesModel.js
@@ -0,0 +1,12 @@
+const connection = require('./connection');
+
+const create = async (itens) => {
+  const { insertedId } = await connection()
+    .then((db) => db.collection('sales')
+      .insertOne({itensSold: itens}));
+  return ({_id: insertedId, itensSold: itens});
+};
+
+module.exports = {
+  create,
+};
