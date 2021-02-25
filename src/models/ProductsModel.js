03b6b951d3cf26abf73b184a62b5c9d6adb026ee
CristianoNunes <cristianonunes99@gmail.com>
2021-02-24 21:47:01 -0300

    Desafio 4 - Tentando descobrir

--- src/models/ProductsModel.js
+++ src/models/ProductsModel.js
@@ -1,6 +1,7 @@
 const connection = require('./connection');
 const { ObjectId } = require('mongodb');
 
+// Desafio 1 - Cadastra um produto
 const createProduct = async (name, quantity) => {
   const { insertedId } = await connection()
     .then((db) => db.collection('products').insertOne({name, quantity}));
@@ -12,15 +13,18 @@ const createProduct = async (name, quantity) => {
   };
 };
 
+// Desafio 2 - Busca todos os produtos
 const getAllProducts = async() => {
   return { products: await connection()
     .then((db) => db.collection('products').find().toArray())};
 };
 
+// Desafio 2 - Busca um produto pelo id
 const findByIdProduct = async(id) => {
   return await connection().then((db) => db.collection('products').findOne(ObjectId(id)));
 };
 
+// Desafio 3 - Atualizar um produto pelo id
 const updateByIdProduct = async(id, name, quantity) => {
   const { insertedId } = await connection()
     .then((db) => db.collection('products')
@@ -32,6 +36,7 @@ const updateByIdProduct = async(id, name, quantity) => {
   };
 };
 
+// Desafio 4 - Deletar um produto pelo id
 const deleteByIdProduct = async(id) => {
   return await connection().then((db) => db.collection('products')
     .deleteOne({_id: ObjectId}));
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,28 +2,33 @@ const ProductController = require('../controllers/ProductController');
 const Product = require('../models/ProductsModel');
 const { validationCreate, validationUpdate, validationId } = require('./Validations');
 
+// Desafio 1 - Cadastra um produto
 const createProduct = async (name, quantity) => {
   const validation = await validationCreate(name, quantity);
   if(validation) return validation;
   return await Product.createProduct(name, quantity);
 };
 
+// Desafio 2 - Busca todos os produtos
 const getAllProducts = async () => {
   return await Product.getAllProducts();
 };
 
+// Desafio 2 - Busca um produto pelo id
 const findByIdProduct = async (id) => {
   const validation = validationId(id);
   if(validation) return validation;
   return await Product.findByIdProduct(id);
 };
 
+// Desafio 3 - Atualizar um produto pelo id
 const updateByIdProduct = async (id, name, quantity) => {
   const validation = await validationUpdate(name, quantity);
   if(validation) return validation;
   return await Product.updateByIdProduct(id, name, quantity);
 };
 
+// Desafio 4 - Deletar um produto pelo id
 const deleteByIdProduct = async (id) => {
   const product = findByIdProduct(id);
   await Product.deleteByIdProduct(id);
