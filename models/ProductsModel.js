f3d64e08c74e81fc72a216fc3f2cec86302bae1a
Alessandra <itsalematos@hotmail.com>
2021-02-25 15:09:11 -0400

    Feat: Create get all and get by id routes

--- models/ProductsModel.js
+++ models/ProductsModel.js
@@ -26,9 +26,9 @@ const getByName = async (name) => {
     .then(db => db.collection('products').findOne({ name }));
 };
 
-const getById = async (name) => {
+const getById = async (id) => {
   return await connection()
-    .then(db => db.collection('products').findOne({ ObjectId }));
+    .then(db => db.collection('products').findOne(ObjectId(id)));
 };
 
 module.exports = {
