4e10dbd65752951bfb8c9d4a3e858e86a36f92cb
Phelipe Ohlsen <phb.ohlsen@gmail.com>
2021-02-25 13:32:47 -0300

    handleError bug fixed

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,6 +1,11 @@
+const handleMessage = require('../utils/dictionary/handleMessage');
+const badRequest = 400;
+
 module.exports = (err, _req, res, _next) => {
-  const error = (typeof err === 'string') ? err : err.message;
+  let error = (typeof err === 'string') ? err : err.message;
+  if (error[0] !== '{' ) error = handleMessage(error, badRequest, 'unexpected_error');
   const { message, status, code } = JSON.parse(error);
+
   res.status(status).json({
     err: { code, message }
   });
