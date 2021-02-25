865178bcc2053e1ea449ba7fbfbe03d83fb90487
LeonardoCavachini <cavachini240@gmail.com>
2021-02-25 19:51:02 -0300

    wrong server

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // local
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; //evaluator
+//const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager'; // local
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; //evaluator
 
 const connection = () => {
   return MongoClient
