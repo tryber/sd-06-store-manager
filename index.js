3b2aa92076a00f765bebd31180283eb7e2e0d1fd
Pedro Duarte <ikbenpedro@gmail.com>
2021-02-26 13:36:45 -0300

    Nodejs Project || Pedro Duarte

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // não remova esse endpoint, e para o avaliador funcionar
+const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const DB_NAME = 'StoreManager'
+
 app.get('/', (_request, response) => {
   response.send();
 });
