6ff66aba4d3a39963d73fb43c74e3735b8b0d917
higoramorim <1810042@aluno.univesp.br>
2021-02-27 20:55:02 -0300

    req 5 20:54

--- models/connection.js
+++ models/connection.js
@@ -3,18 +3,18 @@ const { MongoClient } = require('mongodb');
 let connection;
 
 const DB_NAME = 'StoreManager';
-// const DB_URL = 'mongodb://localhost:27017';
-const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+const DB_URL = 'mongodb://localhost:27017';
+// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
 
 async function getCollection(collectionName) {
   connection =
     connection ||
-    (await MongoClient.connect(DB_URL, {
+    (await MongoClient.connect(MONGO_DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }));
 
-  return connection.db(MONGO_DB_NAME).collection(collectionName);
+  return connection.db(DB_NAME).collection(collectionName);
 }
 
 module.exports = getCollection;
