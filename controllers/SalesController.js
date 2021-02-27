80ea1f9892aa2552916bd250c195c29c93fc1b61
Cássio Rubens <cassiorubensadv@gmail.com>
2021-02-27 10:39:40 -0300

    FEAT: 6 - Crie um endpoint para listar as vendas

--- controllers/SalesController.js
+++ controllers/SalesController.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
+const { ObjectId } = require('mongodb');
 const Sales = require('../models/Sales');
 
 const router = Router();
 
 const SUCCESS = 200;
 const Erro422 = 422;
+const Erro404 = 404;
 const Zero = 0;
 
 
@@ -14,6 +16,7 @@ router.get('/', async (_req, res) => {
   res.status(SUCCESS).json({sales});
 });
 
+// 5
 const validationSales = async (quantity) => {
   // Será validado que não é possível cadastrar vendas com quantidade menor ou iqual a zero
   if (quantity <= Zero) {
@@ -34,20 +37,45 @@ const validationSales = async (quantity) => {
 };
 
 router.post('/', async (req, res) => {
-  const sales = req.body;
+  const itensSold = req.body;
   // forEach não respeita a ordem async/await e Promise.all só funciona se a ordem de execução não for importante.
-  for (const product of sales) {
+  for (const product of itensSold) {
     const err = await validationSales(product.quantity);
     if (err) return res.status(Erro422).json({ err });  
   } // O for..of executa o loop na ordem esperada, aguardando que cada operação await anterior seja concluída antes de passar para a próxima.
   
-  const { insertedId } = await Sales.create(sales);
+  const { insertedId } = await Sales.create(itensSold);
   const salesInserted = {
     _id: insertedId,
-    itensSold: sales
+    itensSold
   };
   
   return res.status(SUCCESS).json(salesInserted);
 });
+// 5
+
+// 6
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  if(!ObjectId.isValid(id)) {
+    return res.status(Erro404).json({
+      err: {
+        code: 'not_found',
+        message: 'Sale not found'
+      }
+    });
+  }
+  const saleId = await Sales.getById(id);
+  if(!saleId) {
+    return res.status(Erro404).json({
+      err: {
+        code: 'not_found',
+        message: 'Sale not found'
+      }
+    });
+  }
+  return res.status(SUCCESS).json(saleId);
+});
+// 6
 
 module.exports = router;
--- models/Sales.js
+++ models/Sales.js
@@ -1,15 +1,24 @@
+const { ObjectId } = require('mongodb');
 const connection = require('./connection');
 
 const getAll = async () => {
   return await connection().then(db => db.collection('sales').find().toArray());
 };
 
-const create = async (sales) => {
+const create = async (itensSold) => {
   return await connection().then(db => 
-    db.collection('sales').insertOne({ sales }));
+    db.collection('sales').insertOne({ itensSold }));
 };
+
+// 6
+const getById = async (id) => {
+  return await connection().then(db => 
+    db.collection('sales').findOne({ _id: ObjectId(id) }));
+};
+// 6
 
 module.exports = {
   getAll,
   create,
+  getById,
 };
