80e83132577feadc0ab4d0b348ff91892daa7d17
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2021-02-28 12:55:29 -0300

    fix: improvement error handling

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,16 @@
+const Boom = require('@hapi/boom');
+
 module.exports = (err, _req, res, _next) => {
-  const { statusCode, payload } = err.output;
-  res
-    .status(statusCode)
-    .json({ err: { code: 'invalid_data', message: payload.message } });
+  const badImplementation = 500;
+  
+  if (Boom.isBoom(err)) {
+    const { statusCode, payload } = err.output;
+    res
+      .status(statusCode)
+      .json({ err: { code: 'invalid_data', message: payload.message } });
+  };
+
+  return res
+    .status(badImplementation)
+    .json({ message: err.message});
 };
