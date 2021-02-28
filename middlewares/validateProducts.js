4cf90bb1d05fed06e6ae57c9436bd3b26d71d709
afornagieri <fornagieri.artur@gmail.com>
2021-02-27 23:04:41 -0300

    Requisito 1 fix eslint issues

--- middlewares/validateProducts.js
+++ middlewares/validateProducts.js
@@ -1,27 +1,42 @@
 
 const { addNewProduct, searchForProductName } = require('../models/productsModel');
 
+const ERROR422 = 422;
+const CREATED = 201;
+const FIVE = 5;
+const ZERO = 0;
+
 const validateProducts = async function (req, res, next) {
 
   const { name, quantity } = req.body;
 
-  /* eslint-disable */
   const getProductByName = await searchForProductName(name);
 
-  if(name.length < 5 ) {
-    res.status(422).json({ err: { code: 'invalid_data', message: '"name" length must be at least 5 characters long'} });
+  if(name.length < FIVE ) {
+    res.status(ERROR422).json(
+      { err:
+        {
+          code: 'invalid_data',
+          message: '"name" length must be at least 5 characters long'
+        }});
   }
 
   if(typeof quantity !== 'number') {
-    res.status(422).json({ err: { code: 'invalid_data', message: '\"quantity\" must be a number'} });
+    res.status(ERROR422).json(
+      { err: { code: 'invalid_data', message: '\"quantity\" must be a number'} });
   }
   
-  if(typeof quantity !== 'number' || quantity <= 0) {
-    res.status(422).json({ err: { code: 'invalid_data', message: '\"quantity\" must be larger than or equal to 1'} });
+  if(typeof quantity !== 'number' || quantity <= ZERO) {
+    res.status(ERROR422).json({
+      err:
+      {
+        code: 'invalid_data',
+        message: '\"quantity\" must be larger than or equal to 1'
+      } });
   }
 
   if(getProductByName.find((ProductName) => ProductName === ProductName )) {
-    res.status(422).json(
+    res.status(ERROR422).json(
       { err:
         {
           code: 'invalid_data',
@@ -31,9 +46,8 @@ const validateProducts = async function (req, res, next) {
 
   if(!getProductByName.find((ProductName) => ProductName === ProductName )) {
     await addNewProduct(name, quantity);
-    res.status(201).json(await searchForProductName(name));
+    res.status(CREATED).json(await searchForProductName(name));
   }
-  /* eslint-enable */ 
   
   next();
 };
