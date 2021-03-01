eee7f25aa81d137b3071523a73912d488b5f74b7
Leandro <leandro.feitoza8018@gmail.com>
2021-02-28 21:40:18 -0300

    temos todo o tempo do mundo

--- api/database/connection.js
+++ api/database/connection.js
@@ -1,7 +1,7 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = process.env.MONGO_DB_URL;
-const DB_NAME = process.env.DB_NAME;
+const MONGO_DB_URL='mongodb://mongodb:27017/StoreManager';
+const DB_NAME='StoreManager';
 
 const connection = () => {
   return MongoClient
