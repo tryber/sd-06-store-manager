b39f19c675544eb1e372116ba90990ee5d1baf03
Daniel Jesus <dev.danieljs@gmail.com>
2021-03-01 19:12:17 -0300

    build: change database to production mode

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-const evalautor = false;
+const evalautor = true;
 const MONGODB_URL = evalautor
   ? 'mongodb://mongodb:27017/StoreManager'
   : 'mongodb://localhost:27017/StoreManager';
