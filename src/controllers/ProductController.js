e8fe51af59efe400af4c14adbd4b77b43dc2f933
ricardorosa-dev <ricardorosa@gmail.com>
2021-03-01 05:17:17 -0300

    Ue, o status deve ser 422 ou 404???

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -10,11 +10,11 @@ const validateProduct = (req, res, next) => {
   
   if (!name || !quantity) res.status(process.env.badRequest)
     .json({ message: 'Product must have the keys "name" and "quantity"' });
-  if (name.length < process.env.five) res.status(process.env.unprocessableEntity)
+  if (name.length < process.env.five) res.status(process.env.notFound)
     .json({ message: '"name" length must be at least 5 characters long' });
-  if (quantity < 1) res.status(process.env.unprocessableEntity)
+  if (quantity < 1) res.status(process.env.notFound)
     .json({ message: '"quantity" must be larger than or equal to 1' });
-  if (typeof quantity !== 'number') res.status(process.env.unprocessableEntity)
+  if (typeof quantity !== 'number') res.status(process.env.notFound)
     .json({ message: '"quantity" must be a number' });
   else {
     next();
@@ -26,10 +26,10 @@ router.post('/', validateProduct, rescue (async (req, res) => {
   const { name, quantity } = req.body;
   const store = await ProductService.create(name, quantity);
   
-  if (!store) return res.status(process.env.unprocessableEntity)
+  if (!store) return res.status(process.env.notFound)
     .json({ message: 'Product already exists' });
   
-  res.status(process.env.ok).json(store);
+  res.status(process.env.created).json(store);
 }));
 
 router.get('/', rescue (async (req, res) => {
