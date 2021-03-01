d7fc8d16e50c5f852effe60926f2c8eaa4f73b32
ricardorosa-dev <ricardorosa@gmail.com>
2021-03-01 05:08:53 -0300

    ah tem que mudar a url :(

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,8 +1,8 @@
 const { MongoClient } = require('mongodb');
 
 //Avaliador
-// const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager'; 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/StoreManager';
+// const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
 const DB_NAME = 'StoreManager';
 
 const connection = async () => {
