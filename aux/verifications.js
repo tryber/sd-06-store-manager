6eb48b0285a42976426ae617f93a2cfe2b80acf7
Kesley Lahoz <namielahoz@gmail.com>
2021-03-02 16:38:32 -0300

    changing verifications

--- aux/verifications.js
+++ aux/verifications.js
@@ -1,33 +1,32 @@
-const UnprocessableEntity = 422;
-// const success = 200;
-// const successCreated = 201;
-// const notFound = 404;
-const zero = 0;
-
+const status_ue = 422;
+const status_c = 201;
+const status_s = 200;
+const status_nf = 404;
 const error = {
   err: {
     code: 'invalid_data',
     message: ''
   }
 };
+const ZERO = 0;
 
 const verifyQuantity = (q) => {
   let message = '';
-  let status = zero;
+  let status = ZERO;
   let isValid = true;
 
   const tQ = typeof(q) === 'string';
-  const lQ = q <= zero;
+  const lQ = q <= ZERO;
 
   if (tQ) {
     message = '"quantity" must be a number';
-    status = UnprocessableEntity;
+    status = status_ue;
     isValid = false;
   }
 
   if (lQ) {
     message = '"quantity" must be larger than or equal to 1';
-    status = UnprocessableEntity;
+    status = status_ue;
     isValid = false;
   }
 
