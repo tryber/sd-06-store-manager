59f471f90919330381d42eb97207b7ef242b962a
ilanribeiro <ilanribeiro@gmail.com>
2021-03-03 16:59:57 -0300

    [ change ] Modificar o arquivo connection, incluindo o nome do DB via variável de ambiente.

--- models/connection.js
+++ models/connection.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb');
 
 // Procedimento ensinado pelo Nato em uma thread de dúvida;
+// const MONGODB_URL = 'mongodb://localhost:27017/StoreManager';
+
 const MONGODB_URL = process.env.IS_LOCAL
   ? 'mongodb://localhost:27017/StoreManager'
   : 'mongodb://127.0.0.1:27017';
-const DB_NAME = 'StoreManager';
+
+const DB_NAME = process.env.DB_NAME;
 
 const connection = () => {
   return MongoClient.connect(MONGODB_URL, {
