795d6aa7af7f5f865130c98c34c9227da5ce6e58
deko possas <andrepossaster@gmail.com>
2021-03-03 15:39:41 -0300

    criando validacao de unico item

--- service/productService.js
+++ service/productService.js
@@ -1,12 +1,20 @@
 const FIVE = 5;
 const UNPROCESSABLE = 422;
+const { getAllProduct } = require('../models/productModel');
 
 const validateName = async (req, res, next) => {
   const { productName } = req.body.name;
   if (!productName || productName.length < FIVE) {
     return res.status(UNPROCESSABLE)
-      .json({ message: '\"name"\ length must be at least 5 characters long'});
+      .json({
+        err: {
+          code: 'invalid_data',
+          message: '\"name"\ length must be at least 5 characters long'
+        }
+      });
   }
+  const productList = await getAllProduct();
+  const checkUnique = await productList.find((product) => product.name === productName);
 };
 
 module.exports = { validateName };
