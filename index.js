a4a48a66187fa1fd5b85af2efa60f8d053d381a6
ilanribeiro <ilanribeiro@gmail.com>
2021-03-03 17:03:58 -0300

    [ Feature ] Criar a rota de produtos e uma rota de teste para verificar funcionamento

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  dotenv = require('dotenv'),
+  productsRouter = require('./controllers/productsRouter');
+
+const
+  app = express(),
+  port = 3000;
+
+dotenv.config();
+app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
-app.get('/hello', (req, res) => res.send('Hello World!'));
+app.use('/products', productsRouter);
+
+app.get('/test', (req, res) => res.send('Hello Project!'));
 app.listen(port, () => console.log(`MyStoreApp active and listening on port ${port}!`));
